4407767e37b8cf00d1bc257feb85b77a40198915
K4 <k4nu420@gmail.com>
2025-12-07 14:43:30 +0100

    config.py initilization

---
+++ config.py
@@ -0,0 +1,14 @@
+# Rozmiar okna
+WINDOW_WIDTH = 800
+WINDOW_HEIGHT = 600
+
+# Siatka
+CELL_SIZE = 20
+
+# FPS
+FPS = 60
+
+# Kolory
+COLOR_BG = (10, 10, 40)       # tło
+COLOR_GRID = (50, 50, 80)     # siatka
+COLOR_TEXT = (220, 220, 220)  # tekst HUD
